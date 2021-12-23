f44cf19b88051d4e67bd7b469ed03360014be1fc
JohnDaytonCC <john.dayton.cc@gmail.com>
2021-12-23 18:42:28 -0300

    Criação da classe Revervation

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -1,5 +1,71 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reservation {
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	Reservation(){
+	}
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomNuber() {
+		return roomNumber;
+	}
+
+	public void setRoomNuber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+
+	public long duration() {
+		long diff = checkout.getTime() - checkin.getTime();
+		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+	}
+	
+	public void updateDates(Date checkin, Date checkout) {
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+	
+	@Override
+	public String toString() {
+		/*
+		StringBuilder sb = new StringBuilder();
+		sb.append("Room "+ roomNumber);
+		sb.append(", check-in: " + sdf.format(checkin));
+		sb.append(", check-out: " + sdf.format(checkout));
+		sb.append(", " + duration() + " nights");
+		return sb.toString();
+		*/
+		
+		return "Room "
+				+ roomNumber
+				+ ", check-in: "
+				+ sdf.format(checkin)
+				+ ", check-out: "
+				+ sdf.format(checkout)
+				+ ", "
+				+ duration()
+				+ " nights";
+	}
 
 }
